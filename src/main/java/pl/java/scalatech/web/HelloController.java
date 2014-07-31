526ea8853bc016345f512591d7b00c1cda72f9f4
Slawomir Borowiec <przodownik@tlen.pl>
2014-07-31 18:25:30 +0200

    hello controller added

--- src/main/java/pl/java/scalatech/web/HelloController.java
+++ src/main/java/pl/java/scalatech/web/HelloController.java
@@ -1,7 +1,20 @@
 package pl.java.scalatech.web;
 
+import java.time.LocalDate;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,5 +30,40 @@ public class HelloController {
     @RequestMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
     public String sayHello() {
         return RESPONSE_BODY;
+    }
+    @RequestMapping(value = "/json", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public SimpleModel getJson() {
+        return new SimpleModel("przodownik", LocalDate.of(2010, 2, 4));
+    }
+    
+    @RequestMapping(value = "/xml", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
+    public SimpleModel getXml() {
+        return new SimpleModel("przodownik", LocalDate.of(2010, 2, 4));
+    }
+    
+    
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @XmlRootElement
+    public static class SimpleModel {
+        @Getter
+        private String name;
+        @Getter
+        @XmlJavaTypeAdapter( LocalDateAdapter.class )
+        private LocalDate date;
+    }
+    
+    public static class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+        @Override
+        public LocalDate unmarshal(String v) throws Exception {
+            return LocalDate.parse(v);
+        }
+
+        @Override
+        public String marshal(LocalDate v) throws Exception {
+            return v.toString();
+        }
+
     }
 }
