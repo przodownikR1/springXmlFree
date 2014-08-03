8f44ac34ee931e5ae23d4522c606c195fb96ab07
Slawomir Borowiec <przodownik@tlen.pl>
2014-08-04 01:14:17 +0200

    repair spock test

--- src/test/groovy/pl/java/scalatech/test/spock/InvoiceTest.groovy
+++ src/test/groovy/pl/java/scalatech/test/spock/InvoiceTest.groovy
@@ -1,18 +1,15 @@
 package pl.java.scalatech.test.spock
 
-import pl.java.scalatech.domain.Invoice;
-import pl.java.scalatech.domain.InvoiceType;
-import spock.lang.Specification;
+import pl.java.scalatech.domain.Invoice
+import spock.lang.Specification
 
 class InvoiceTest extends Specification {
     Invoice invoice = new Invoice();
-    
+
     def "create invoice"() {
-       
+
         expect:
-          
-          invoice.type == InvoiceType.BUSINESS;
-        
-        
+
+        invoice.type == null;
     }
 }
