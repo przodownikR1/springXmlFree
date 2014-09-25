d4d5c66645291b75b593794c1ccf7a4656fc9d66
Slawomir Borowiec <przodownik@tlen.pl>
2014-09-25 16:15:15 +0200

    shallowETag added

--- src/main/java/pl/java/scalatech/initializer/WebInitializer.java
+++ src/main/java/pl/java/scalatech/initializer/WebInitializer.java
@@ -1,22 +1,27 @@
 package pl.java.scalatech.initializer;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /**
- * @author Sławomir Borowiec 
- * Module name : spring4WithoutXml
- * Creating time :  31 lip 2014 17:55:29
- 
+ * @author Sławomir Borowiec
+ *         Module name : spring4WithoutXml
+ *         Creating time : 31 lip 2014 17:55:29
  */
 public class WebInitializer implements WebApplicationInitializer {
 
@@ -28,11 +33,14 @@ public class WebInitializer implements WebApplicationInitializer {
         WebApplicationContext context = getContext();
         servletContext.addListener(new ContextLoaderListener(context));
         servletContext.setInitParameter("defaultHtmlEscape", "true");
+
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping(MAPPING_URL);
-      
+
         encodingFilter(servletContext);
+        registerShallowEtagHeaderFilter(servletContext);
+        registerHiddenHttpMethodFilter(servletContext);
 
     }
 
@@ -43,12 +51,22 @@ public class WebInitializer implements WebApplicationInitializer {
     }
 
     private void encodingFilter(ServletContext servletContext) {
-        FilterRegistration.Dynamic fr = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
-        fr.setInitParameter("encoding", "UTF-8");
-        fr.setInitParameter("forceEncoding", "true");
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        FilterRegistration.Dynamic fr = servletContext.addFilter("encodingFilter", characterEncodingFilter);
         fr.addMappingForUrlPatterns(null, true, "/*");
     }
 
+    private FilterRegistration.Dynamic registerShallowEtagHeaderFilter(ServletContext servletContext) {
+        FilterRegistration.Dynamic shallowEtagHeaderFilter = servletContext.addFilter("shallowEtagHeaderFilter", new ShallowEtagHeaderFilter());
+        shallowEtagHeaderFilter.addMappingForServletNames(EnumSet.allOf(DispatcherType.class), true, "dispatcher");
+        return shallowEtagHeaderFilter;
+    }
+
+    private FilterRegistration.Dynamic registerHiddenHttpMethodFilter(ServletContext servletContext) {
+        FilterRegistration.Dynamic hiddenHttpMethodFilter = servletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter());
+        hiddenHttpMethodFilter.addMappingForServletNames(EnumSet.allOf(DispatcherType.class), true, "dispatcher");
+        return hiddenHttpMethodFilter;
+    }
 }
-
-
